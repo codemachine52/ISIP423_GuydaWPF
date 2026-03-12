5bcd33b1d1da0362b533222c5565559c9cae1c32
Stasyamr4 <anastasiamura04@mail.ru>
2026-03-12 17:34:03 +0300

    реализован поиск фильма по названию

--- WpfApp1/Pages/Page1.xaml.cs
+++ WpfApp1/Pages/Page1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Security.Cryptography;
@@ -12,13 +13,16 @@ namespace WpfApp1.Pages
 {
     public partial class Page1 : Page
     {
-        private List<Film> allMovies = Core.Context.Film.ToList();
+        private List<Film> _allMovies;              // все фильмы из БД
+        private ObservableCollection<Film> _displayedMovies; // для отображения в ListBox
         Client User;
 
         public Page1()
         {
             InitializeComponent();
-            listBox.ItemsSource = allMovies;
+            _allMovies = Core.Context.Film.ToList();
+            _displayedMovies = new ObservableCollection<Film>(_allMovies);
+            listBox.ItemsSource = _displayedMovies;
         }
 
         public Page1(Client us) : this()
@@ -43,6 +47,30 @@ namespace WpfApp1.Pages
             Button btn = sender as Button;
             Film thisFilm = btn.DataContext as Film;
             NavigationService.Navigate(new Page5(thisFilm));
+        }
+        private void FindingFilm()
+        {
+            string searchText = FindFilm.Text.Trim().ToLower();
+            _displayedMovies.Clear();
+            if (string.IsNullOrEmpty(searchText))
+            {
+                // Если поиск пустой – показываем все фильмы
+                foreach (var film in _allMovies)
+                    _displayedMovies.Add(film);
+                return;
+            }
+
+            var foundFilms = _allMovies.Where(item =>
+                item.Name.Trim().ToLower().Contains(searchText)).ToList();
+
+            // Если фильм найден, добавляем его в ListBox
+            foreach (var film in foundFilms)
+                _displayedMovies.Add(film);
+        }
+
+        private void FindFilm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FindingFilm();
         }
     }
 }
